1b2cf0f3ca6f2439c9ee5eb5fc52f15b428d4b4c
SUPERKEON\keon <up2sky7@naver.com>
2021-11-25 21:38:14 +0900

    Flower Gardens Added

--- app/src/main/java/com/example/garden/GPSFragment.java
+++ app/src/main/java/com/example/garden/GPSFragment.java
@@ -32,9 +32,30 @@ public class GPSFragment extends Fragment {
 
         //127.028002, 37.4945470
         MapPoint mapPoint = MapPoint.mapPointWithGeoCoord(37.4945470,127.028002);
-        MapPoint mapPoint2 = MapPoint.mapPointWithGeoCoord(37.4946800,127.028502);
-        MapPoint mapPoint3 = MapPoint.mapPointWithGeoCoord(37.4950000,127.028002);
+        MapPoint mapPoint2 = MapPoint.mapPointWithGeoCoord(37.4960625,127.0290685);
+        MapPoint mapPoint3 = MapPoint.mapPointWithGeoCoord(37.4937471,127.0302915);
 
+        MapPoint mapPoint4 = MapPoint.mapPointWithGeoCoord(37.4989055,127.028081);
+        MapPoint mapPoint5 = MapPoint.mapPointWithGeoCoord(37.4962497,127.0329308);
+        MapPoint mapPoint6 = MapPoint.mapPointWithGeoCoord(37.4948367,127.0346260);
+
+        MapPoint mapPoint7 = MapPoint.mapPointWithGeoCoord(37.4969948,127.0299518);
+        MapPoint mapPoint8 = MapPoint.mapPointWithGeoCoord(37.4954796,127.0303380);
+        MapPoint mapPoint9 = MapPoint.mapPointWithGeoCoord(37.4953941,127.02720455);
+
+        MapPoint mapPoint10 = MapPoint.mapPointWithGeoCoord(37.4990628,127.0316141);
+        MapPoint mapPoint11 = MapPoint.mapPointWithGeoCoord(37.5003566,127.0331161);
+        MapPoint mapPoint12 = MapPoint.mapPointWithGeoCoord(37.4906015,127.0318715);
+
+        MapPoint mapPoint13 = MapPoint.mapPointWithGeoCoord(37.4954071,127.0197569);
+        MapPoint mapPoint14 = MapPoint.mapPointWithGeoCoord(37.4934449,127.0186038);
+        MapPoint mapPoint15 = MapPoint.mapPointWithGeoCoord(37.4899055,127.0217901);
+
+        MapPoint mapPoint16 = MapPoint.mapPointWithGeoCoord(37.4863445,127.0351979);
+        MapPoint mapPoint17 = MapPoint.mapPointWithGeoCoord(37.4863105,127.04322316);
+        MapPoint mapPoint18 = MapPoint.mapPointWithGeoCoord(37.49327411, 127.01356866);
+
+        MapPoint mapPoint19 = MapPoint.mapPointWithGeoCoord(37.4878338, 127.0173449);
         //지도의 중심점을 특정 위치로 설정 확대 레벨 설정 (값이 작을수록 더 확대된다)
         mapView.setMapCenterPoint(mapPoint,true);
         mapView.setZoomLevel(1,true);
@@ -52,7 +73,7 @@ public class GPSFragment extends Fragment {
         customMarker.setCustomImageAnchor(0.5f, 1.0f); // 마커 이미지중 기준이 되는 위치(앵커포인트) 지정 - 마커 이미지 좌측 상단 기준 x(0.0f ~ 1.0f), y(0.0f ~ 1.0f) 값.
         mapView.addPOIItem(customMarker);
 
-        customMarker.setItemName("선화 꽃집");
+        customMarker.setItemName("김미경 플라워");
         customMarker.setTag(1);
         customMarker.setMapPoint(mapPoint2);
         customMarker.setMarkerType(MapPOIItem.MarkerType.CustomImage); // 마커타입을 커스텀 마커로 지정.
@@ -61,14 +82,157 @@ public class GPSFragment extends Fragment {
         customMarker.setCustomImageAnchor(0.5f, 1.0f); // 마커 이미지중 기준이 되는 위치(앵커포인트) 지정 - 마커 이미지 좌측 상단 기준 x(0.0f ~ 1.0f), y(0.0f ~ 1.0f) 값.
         mapView.addPOIItem(customMarker);
 
-        customMarker.setItemName("Garden Flower");
+        customMarker.setItemName("스노우폭스 플라워");
         customMarker.setTag(1);
         customMarker.setMapPoint(mapPoint3);
         customMarker.setMarkerType(MapPOIItem.MarkerType.CustomImage); // 마커타입을 커스텀 마커로 지정.
         customMarker.setCustomImageResourceId(R.drawable.plant_point); // 마커 이미지.
         customMarker.setCustomImageAutoscale(false); // hdpi, xhdpi 등 안드로이드 플랫폼의 스케일을 사용할 경우 지도 라이브러리의 스케일 기능을 꺼줌.
         customMarker.setCustomImageAnchor(0.5f, 1.0f); // 마커 이미지중 기준이 되는 위치(앵커포인트) 지정 - 마커 이미지 좌측 상단 기준 x(0.0f ~ 1.0f), y(0.0f ~ 1.0f) 값.
+        mapView.addPOIItem(customMarker);
 
+        customMarker.setItemName("삼성 플라워");
+        customMarker.setTag(1);
+        customMarker.setMapPoint(mapPoint4);
+        customMarker.setMarkerType(MapPOIItem.MarkerType.CustomImage); // 마커타입을 커스텀 마커로 지정.
+        customMarker.setCustomImageResourceId(R.drawable.plant_point); // 마커 이미지.
+        customMarker.setCustomImageAutoscale(false); // hdpi, xhdpi 등 안드로이드 플랫폼의 스케일을 사용할 경우 지도 라이브러리의 스케일 기능을 꺼줌.
+        customMarker.setCustomImageAnchor(0.5f, 1.0f); // 마커 이미지중 기준이 되는 위치(앵커포인트) 지정 - 마커 이미지 좌측 상단 기준 x(0.0f ~ 1.0f), y(0.0f ~ 1.0f) 값.
+        mapView.addPOIItem(customMarker);
+
+        customMarker.setItemName("다경 플라워");
+        customMarker.setTag(1);
+        customMarker.setMapPoint(mapPoint5);
+        customMarker.setMarkerType(MapPOIItem.MarkerType.CustomImage); // 마커타입을 커스텀 마커로 지정.
+        customMarker.setCustomImageResourceId(R.drawable.plant_point); // 마커 이미지.
+        customMarker.setCustomImageAutoscale(false); // hdpi, xhdpi 등 안드로이드 플랫폼의 스케일을 사용할 경우 지도 라이브러리의 스케일 기능을 꺼줌.
+        customMarker.setCustomImageAnchor(0.5f, 1.0f); // 마커 이미지중 기준이 되는 위치(앵커포인트) 지정 - 마커 이미지 좌측 상단 기준 x(0.0f ~ 1.0f), y(0.0f ~ 1.0f) 값.
+        mapView.addPOIItem(customMarker);
+
+        customMarker.setItemName("라비쉬 플라워");
+        customMarker.setTag(1);
+        customMarker.setMapPoint(mapPoint6);
+        customMarker.setMarkerType(MapPOIItem.MarkerType.CustomImage); // 마커타입을 커스텀 마커로 지정.
+        customMarker.setCustomImageResourceId(R.drawable.plant_point); // 마커 이미지.
+        customMarker.setCustomImageAutoscale(false); // hdpi, xhdpi 등 안드로이드 플랫폼의 스케일을 사용할 경우 지도 라이브러리의 스케일 기능을 꺼줌.
+        customMarker.setCustomImageAnchor(0.5f, 1.0f); // 마커 이미지중 기준이 되는 위치(앵커포인트) 지정 - 마커 이미지 좌측 상단 기준 x(0.0f ~ 1.0f), y(0.0f ~ 1.0f) 값.
+        mapView.addPOIItem(customMarker);
+
+        customMarker.setItemName("THE 꽃장수");
+        customMarker.setTag(1);
+        customMarker.setMapPoint(mapPoint7);
+        customMarker.setMarkerType(MapPOIItem.MarkerType.CustomImage); // 마커타입을 커스텀 마커로 지정.
+        customMarker.setCustomImageResourceId(R.drawable.plant_point); // 마커 이미지.
+        customMarker.setCustomImageAutoscale(false); // hdpi, xhdpi 등 안드로이드 플랫폼의 스케일을 사용할 경우 지도 라이브러리의 스케일 기능을 꺼줌.
+        customMarker.setCustomImageAnchor(0.5f, 1.0f); // 마커 이미지중 기준이 되는 위치(앵커포인트) 지정 - 마커 이미지 좌측 상단 기준 x(0.0f ~ 1.0f), y(0.0f ~ 1.0f) 값.
+        mapView.addPOIItem(customMarker);
+
+        customMarker.setItemName("소예 꽃과 정원");
+        customMarker.setTag(1);
+        customMarker.setMapPoint(mapPoint8);
+        customMarker.setMarkerType(MapPOIItem.MarkerType.CustomImage); // 마커타입을 커스텀 마커로 지정.
+        customMarker.setCustomImageResourceId(R.drawable.plant_point); // 마커 이미지.
+        customMarker.setCustomImageAutoscale(false); // hdpi, xhdpi 등 안드로이드 플랫폼의 스케일을 사용할 경우 지도 라이브러리의 스케일 기능을 꺼줌.
+        customMarker.setCustomImageAnchor(0.5f, 1.0f); // 마커 이미지중 기준이 되는 위치(앵커포인트) 지정 - 마커 이미지 좌측 상단 기준 x(0.0f ~ 1.0f), y(0.0f ~ 1.0f) 값.
+        mapView.addPOIItem(customMarker);
+
+        customMarker.setItemName("러브 투 플라워");
+        customMarker.setTag(1);
+        customMarker.setMapPoint(mapPoint9);
+        customMarker.setMarkerType(MapPOIItem.MarkerType.CustomImage); // 마커타입을 커스텀 마커로 지정.
+        customMarker.setCustomImageResourceId(R.drawable.plant_point); // 마커 이미지.
+        customMarker.setCustomImageAutoscale(false); // hdpi, xhdpi 등 안드로이드 플랫폼의 스케일을 사용할 경우 지도 라이브러리의 스케일 기능을 꺼줌.
+        customMarker.setCustomImageAnchor(0.5f, 1.0f); // 마커 이미지중 기준이 되는 위치(앵커포인트) 지정 - 마커 이미지 좌측 상단 기준 x(0.0f ~ 1.0f), y(0.0f ~ 1.0f) 값.
+        mapView.addPOIItem(customMarker);
+
+        customMarker.setItemName("후로라 플라워");
+        customMarker.setTag(1);
+        customMarker.setMapPoint(mapPoint10);
+        customMarker.setMarkerType(MapPOIItem.MarkerType.CustomImage); // 마커타입을 커스텀 마커로 지정.
+        customMarker.setCustomImageResourceId(R.drawable.plant_point); // 마커 이미지.
+        customMarker.setCustomImageAutoscale(false); // hdpi, xhdpi 등 안드로이드 플랫폼의 스케일을 사용할 경우 지도 라이브러리의 스케일 기능을 꺼줌.
+        customMarker.setCustomImageAnchor(0.5f, 1.0f); // 마커 이미지중 기준이 되는 위치(앵커포인트) 지정 - 마커 이미지 좌측 상단 기준 x(0.0f ~ 1.0f), y(0.0f ~ 1.0f) 값.
+        mapView.addPOIItem(customMarker);
+
+        customMarker.setItemName("그린 앤 플라워");
+        customMarker.setTag(1);
+        customMarker.setMapPoint(mapPoint11);
+        customMarker.setMarkerType(MapPOIItem.MarkerType.CustomImage); // 마커타입을 커스텀 마커로 지정.
+        customMarker.setCustomImageResourceId(R.drawable.plant_point); // 마커 이미지.
+        customMarker.setCustomImageAutoscale(false); // hdpi, xhdpi 등 안드로이드 플랫폼의 스케일을 사용할 경우 지도 라이브러리의 스케일 기능을 꺼줌.
+        customMarker.setCustomImageAnchor(0.5f, 1.0f); // 마커 이미지중 기준이 되는 위치(앵커포인트) 지정 - 마커 이미지 좌측 상단 기준 x(0.0f ~ 1.0f), y(0.0f ~ 1.0f) 값.
+        mapView.addPOIItem(customMarker);
+
+        customMarker.setItemName("가든 플라워");
+        customMarker.setTag(1);
+        customMarker.setMapPoint(mapPoint12);
+        customMarker.setMarkerType(MapPOIItem.MarkerType.CustomImage); // 마커타입을 커스텀 마커로 지정.
+        customMarker.setCustomImageResourceId(R.drawable.plant_point); // 마커 이미지.
+        customMarker.setCustomImageAutoscale(false); // hdpi, xhdpi 등 안드로이드 플랫폼의 스케일을 사용할 경우 지도 라이브러리의 스케일 기능을 꺼줌.
+        customMarker.setCustomImageAnchor(0.5f, 1.0f); // 마커 이미지중 기준이 되는 위치(앵커포인트) 지정 - 마커 이미지 좌측 상단 기준 x(0.0f ~ 1.0f), y(0.0f ~ 1.0f) 값.
+        mapView.addPOIItem(customMarker);
+
+        customMarker.setItemName("락 플라워");
+        customMarker.setTag(1);
+        customMarker.setMapPoint(mapPoint13);
+        customMarker.setMarkerType(MapPOIItem.MarkerType.CustomImage); // 마커타입을 커스텀 마커로 지정.
+        customMarker.setCustomImageResourceId(R.drawable.plant_point); // 마커 이미지.
+        customMarker.setCustomImageAutoscale(false); // hdpi, xhdpi 등 안드로이드 플랫폼의 스케일을 사용할 경우 지도 라이브러리의 스케일 기능을 꺼줌.
+        customMarker.setCustomImageAnchor(0.5f, 1.0f); // 마커 이미지중 기준이 되는 위치(앵커포인트) 지정 - 마커 이미지 좌측 상단 기준 x(0.0f ~ 1.0f), y(0.0f ~ 1.0f) 값.
+        mapView.addPOIItem(customMarker);
+
+        customMarker.setItemName("플라워 119");
+        customMarker.setTag(1);
+        customMarker.setMapPoint(mapPoint14);
+        customMarker.setMarkerType(MapPOIItem.MarkerType.CustomImage); // 마커타입을 커스텀 마커로 지정.
+        customMarker.setCustomImageResourceId(R.drawable.plant_point); // 마커 이미지.
+        customMarker.setCustomImageAutoscale(false); // hdpi, xhdpi 등 안드로이드 플랫폼의 스케일을 사용할 경우 지도 라이브러리의 스케일 기능을 꺼줌.
+        customMarker.setCustomImageAnchor(0.5f, 1.0f); // 마커 이미지중 기준이 되는 위치(앵커포인트) 지정 - 마커 이미지 좌측 상단 기준 x(0.0f ~ 1.0f), y(0.0f ~ 1.0f) 값.
+        mapView.addPOIItem(customMarker);
+
+        customMarker.setItemName("베아티스 플라워");
+        customMarker.setTag(1);
+        customMarker.setMapPoint(mapPoint15);
+        customMarker.setMarkerType(MapPOIItem.MarkerType.CustomImage); // 마커타입을 커스텀 마커로 지정.
+        customMarker.setCustomImageResourceId(R.drawable.plant_point); // 마커 이미지.
+        customMarker.setCustomImageAutoscale(false); // hdpi, xhdpi 등 안드로이드 플랫폼의 스케일을 사용할 경우 지도 라이브러리의 스케일 기능을 꺼줌.
+        customMarker.setCustomImageAnchor(0.5f, 1.0f); // 마커 이미지중 기준이 되는 위치(앵커포인트) 지정 - 마커 이미지 좌측 상단 기준 x(0.0f ~ 1.0f), y(0.0f ~ 1.0f) 값.
+        mapView.addPOIItem(customMarker);
+
+        customMarker.setItemName("월드 플라워");
+        customMarker.setTag(1);
+        customMarker.setMapPoint(mapPoint16);
+        customMarker.setMarkerType(MapPOIItem.MarkerType.CustomImage); // 마커타입을 커스텀 마커로 지정.
+        customMarker.setCustomImageResourceId(R.drawable.plant_point); // 마커 이미지.
+        customMarker.setCustomImageAutoscale(false); // hdpi, xhdpi 등 안드로이드 플랫폼의 스케일을 사용할 경우 지도 라이브러리의 스케일 기능을 꺼줌.
+        customMarker.setCustomImageAnchor(0.5f, 1.0f); // 마커 이미지중 기준이 되는 위치(앵커포인트) 지정 - 마커 이미지 좌측 상단 기준 x(0.0f ~ 1.0f), y(0.0f ~ 1.0f) 값.
+        mapView.addPOIItem(customMarker);
+
+        customMarker.setItemName("플러스 플라워");
+        customMarker.setTag(1);
+        customMarker.setMapPoint(mapPoint17);
+        customMarker.setMarkerType(MapPOIItem.MarkerType.CustomImage); // 마커타입을 커스텀 마커로 지정.
+        customMarker.setCustomImageResourceId(R.drawable.plant_point); // 마커 이미지.
+        customMarker.setCustomImageAutoscale(false); // hdpi, xhdpi 등 안드로이드 플랫폼의 스케일을 사용할 경우 지도 라이브러리의 스케일 기능을 꺼줌.
+        customMarker.setCustomImageAnchor(0.5f, 1.0f); // 마커 이미지중 기준이 되는 위치(앵커포인트) 지정 - 마커 이미지 좌측 상단 기준 x(0.0f ~ 1.0f), y(0.0f ~ 1.0f) 값.
+        mapView.addPOIItem(customMarker);
+
+        customMarker.setItemName("장미 플라워");
+        customMarker.setTag(1);
+        customMarker.setMapPoint(mapPoint18);
+        customMarker.setMarkerType(MapPOIItem.MarkerType.CustomImage); // 마커타입을 커스텀 마커로 지정.
+        customMarker.setCustomImageResourceId(R.drawable.plant_point); // 마커 이미지.
+        customMarker.setCustomImageAutoscale(false); // hdpi, xhdpi 등 안드로이드 플랫폼의 스케일을 사용할 경우 지도 라이브러리의 스케일 기능을 꺼줌.
+        customMarker.setCustomImageAnchor(0.5f, 1.0f); // 마커 이미지중 기준이 되는 위치(앵커포인트) 지정 - 마커 이미지 좌측 상단 기준 x(0.0f ~ 1.0f), y(0.0f ~ 1.0f) 값.
+        mapView.addPOIItem(customMarker);
+
+        customMarker.setItemName("에덴꽃농원");
+        customMarker.setTag(1);
+        customMarker.setMapPoint(mapPoint18);
+        customMarker.setMarkerType(MapPOIItem.MarkerType.CustomImage); // 마커타입을 커스텀 마커로 지정.
+        customMarker.setCustomImageResourceId(R.drawable.plant_point); // 마커 이미지.
+        customMarker.setCustomImageAutoscale(false); // hdpi, xhdpi 등 안드로이드 플랫폼의 스케일을 사용할 경우 지도 라이브러리의 스케일 기능을 꺼줌.
+        customMarker.setCustomImageAnchor(0.5f, 1.0f); // 마커 이미지중 기준이 되는 위치(앵커포인트) 지정 - 마커 이미지 좌측 상단 기준 x(0.0f ~ 1.0f), y(0.0f ~ 1.0f) 값.
         mapView.addPOIItem(customMarker);
 
         return view;
