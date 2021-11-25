3ac8d20071d87bb7addf13ede06acb95e1fe777d
Euna Ahn <eunaahn2001@icloud.com>
2021-11-25 16:19:48 +0900

    피드 뒤로가기/사진추가

--- app/src/main/java/com/example/garden/HomePostActivity.java
+++ app/src/main/java/com/example/garden/HomePostActivity.java
@@ -1,6 +1,7 @@
 package com.example.garden;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Color;
 import android.media.Image;
 import android.os.Bundle;
@@ -11,12 +12,15 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HomePostActivity extends Activity {
+    private ImageButton btnBack2;
+    private ImageButton btnToStore;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_post);
 
-        ImageButton btnBack2 = findViewById(R.id.btnBack2);
+        btnBack2 = findViewById(R.id.btnBack2);
         btnBack2.setColorFilter(Color.parseColor("#000000"));
         btnBack2.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,11 +29,12 @@ public class HomePostActivity extends Activity {
             }
         });
 
-        ImageButton btnToStore = findViewById(R.id.btnToStore);
+        btnToStore = findViewById(R.id.btnToStore);
         btnToStore.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //관련 스토어화면으로 이동
+                Intent intent = new Intent(getApplicationContext(), Product2Activity.class);
+                startActivity(intent);
             }
         });
     }
